6250d1307a8755a3b42e43487e011089e9bf9016
kerfans <kerfans@sina.com>
2017-05-10 14:09:49 +0800

    ci

---
+++ application/views/cache/942f1bdbcddb8c4687b7f5e59f9e726ae8b5fc12.test.html.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-05-10 14:07:34
+  from "D:\wamp\www\www.yaofang.cn\application\views\test.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5912ae26e96ae1_73077944',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'dea80b72720e0c60d44778cfa5050e36df3d326d' => 
+    array (
+      0 => 'D:\\wamp\\www\\www.yaofang.cn\\application\\views\\test.html',
+      1 => 1494394902,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 60,
+),true)) {
+function content_5912ae26e96ae1_73077944 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+ci 3.1.4 结合 smarty 3.1.2 配置成功
+
+	<p>123</p>
+	<p>234</p>
+	<p>567</p>
+
+</body>
+</html><?php }
+}
